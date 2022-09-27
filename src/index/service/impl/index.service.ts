db9840728ad9a76703ccb9fb9a68f22a248eccff
Eric Moore <EricGittaMoore@duck.com>
2022-09-27 09:58:14 +0800

    fix: 日常打卡重复提交

--- src/index/service/impl/index.service.ts
+++ src/index/service/impl/index.service.ts
@@ -111,7 +111,7 @@ export class IndexService {
     for (const signConfigElement of this.signConfig) {
       this.accountData = signConfigElement.account;
       if (translateFn) yield translateFn(signConfigElement[key]);
-      yield signConfigElement[key];
+      else yield signConfigElement[key];
     }
   }
 }
